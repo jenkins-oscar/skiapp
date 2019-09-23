77455de5516f1e3c83a8ed63f64d31d30f947809
sharepointoscar <me@sharepointoscar.com>
2019-09-23 11:56:35 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview 
+
+This is a simple NodeJS application that uses Bootstrap UI. It listens on localhost:8080.  
---
+++ tests/test_server.js
@@ -0,0 +1,23 @@
+var chai =require('chai'),
+chaiHttp= require('chai-http'),
+ app =  require('../server'),
+ should = chai.should(),
+ expect  = require('chai').expect;
+
+ chai.use(chaiHttp);
+
+
+describe("GET Home page", () => {
+    // Test to get all students record
+    it("Should return a 200 ok response", (done) => {
+         chai.request(app)
+             .get('/')
+             .end((err, res) => {
+                 res.should.have.status(200);
+                 res.should.be.html;
+                 //res.body.should.be.a('object');
+                 done();
+              });
+     });
+ 
+});
